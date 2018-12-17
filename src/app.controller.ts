c4c31dc9c4e03d8ce85bc9a51ffc7b130cd89c65
rezolan <alexei.kravchenko@ecocoach.com>
2018-12-17 19:22:24 +0200

    'lalala'

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return 'hello world';
+  }
+
+	@Get('good-bye')
+	getGoodbye(): string {
+		return this.appService.getHello();
+	}
+
+	@Get('get-number/:id')
+	getNumber(@Param('id') id: string): string {
+		return `your number is ${id}`;
+	}
+
+	@Post()
+	postMain(@Body() data: any): string {
+		return {info: 'your data in data key', ...data};
+	}
+}
